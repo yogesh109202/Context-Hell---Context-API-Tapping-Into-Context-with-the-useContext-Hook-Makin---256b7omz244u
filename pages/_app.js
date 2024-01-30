d6238883e9c3d9c1e0e12135d6d302071fb8aa66
yogesh109202 <128414694+yogesh109202@users.noreply.github.com>
2024-01-30 22:31:14 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,22 @@
 // App.js
 import React from 'react';
-
+import { ThemeProvider } from '../contexts/ThemeContext';
+import { LanguageProvider } from '../contexts/LanguageContext';
+import { UserProvider } from '../contexts/UserContext';
+import { ConfigProvider } from '../contexts/ConfigContext';
 import MyComponent from '../components/MyComponent';
 
 function App() {
   return (
+    <ThemeProvider>
+      <LanguageProvider>
+        <UserProvider>
+          <ConfigProvider>
             <MyComponent />
+          </ConfigProvider>
+        </UserProvider>
+      </LanguageProvider>
+    </ThemeProvider>
   );
 }
 
